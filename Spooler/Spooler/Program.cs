fd1165e4648d5ea31db73e2cb1e0af8cc45f2958
Magnumv44 <vitaliy1984@gmail.com>
2023-01-17 10:25:08 +0200

    Додавання коду программи.

---
+++ Spooler/Spooler/Program.cs
@@ -0,0 +1,55 @@
+﻿using Spooler.Properties;
+using System.Diagnostics;
+using System.ServiceProcess;
+
+ServiceController spoolService = new ServiceController("spooler");
+
+EventLog wrEventLog = new EventLog("Application");
+
+void EventLogWrite(string message, EventLogEntryType type, int codeEventId)
+{
+    string sourceForLog = "SpoolerRestarter";
+    wrEventLog.Source = sourceForLog;
+    wrEventLog.WriteEntry(message, type, codeEventId);
+}
+
+try
+{
+    switch (spoolService.Status)
+    {
+        case ServiceControllerStatus.Running:
+            spoolService.Stop();
+            spoolService.WaitForStatus(ServiceControllerStatus.Stopped);
+            spoolService.Start();
+            spoolService.WaitForStatus(ServiceControllerStatus.Running);
+
+            EventLogWrite(Resources.Restarted, EventLogEntryType.Warning, 150);
+            break;
+        case ServiceControllerStatus.Stopped:
+            spoolService.Start();
+            spoolService.WaitForStatus(ServiceControllerStatus.Running);
+
+            EventLogWrite(Resources.Started, EventLogEntryType.Warning, 150);
+            break;
+        case ServiceControllerStatus.Paused:
+            spoolService.Start();
+            spoolService.WaitForStatus(ServiceControllerStatus.Running);
+
+            EventLogWrite(Resources.Started, EventLogEntryType.Warning, 150);
+            break;
+        default:
+            spoolService.Stop();
+            spoolService.WaitForStatus(ServiceControllerStatus.Stopped);
+            spoolService.Start();
+            spoolService.WaitForStatus(ServiceControllerStatus.Running);
+
+            EventLogWrite(Resources.UnknownRestarted, EventLogEntryType.Warning, 150);
+            break;
+    }
+}
+catch (InvalidOperationException)
+{
+    EventLogWrite(Resources.UnknownError, EventLogEntryType.Error, 160);
+}
+
+spoolService.Close();
